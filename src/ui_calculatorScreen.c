9a6486e231b32ceea9d066feb6190a1e646445df
George Ngigi <ngigegeorge023@gmail.com>
2024-07-20 04:39:44 +0300

    Calculator UI designed.

--- src/ui_calculatorScreen.c
+++ src/ui_calculatorScreen.c
@@ -4,6 +4,11 @@
 // Project name: s3-phone
 
 #include "ui.h"
+lv_obj_t *calculationArea;
+lv_obj_t *calculationResults;
+char calculationTokens[] = {'(', ')', '*', '/', '+', '-'};
+
+static void evtComputeCalculations(lv_event_t *e);
 
 void ui_calculatorScreen_screen_init(void) {
   ui_calculatorScreen = lv_obj_create(NULL);
@@ -22,5 +27,73 @@ void ui_calculatorScreen_screen_init(void) {
   lv_obj_set_style_border_opa(ui_panelCalculatorMain, 0,
                               LV_PART_MAIN | LV_STATE_DEFAULT);
 
+  static const char *kb_map[] = {"1",  "2",  "3",  LV_SYMBOL_BACKSPACE,
+                                 "\n", "4",  "5",  "6",
+                                 "/",  "\n", "7",  "8",
+                                 "9",  "*",  "\n", ".",
+                                 "0",  "C",  "+",  "\n",
+                                 "(",  "=",  ")",  "-",
+                                 NULL};
+
+  static const lv_buttonmatrix_ctrl_t kb_ctrl[] = {
+      4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 8, 2, 4};
+
+  lv_obj_t *kb = lv_keyboard_create(ui_panelCalculatorMain);
+  lv_keyboard_set_map(kb, LV_KEYBOARD_MODE_USER_1, kb_map, kb_ctrl);
+  lv_keyboard_set_mode(kb, LV_KEYBOARD_MODE_USER_1);
+  lv_obj_set_width(kb, 240);
+  lv_obj_set_height(kb, 185);
+  lv_obj_set_align(kb, LV_ALIGN_TOP_LEFT);
+  lv_obj_set_y(kb, 95);
+  lv_obj_set_x(kb, -15);
+
+  calculationArea = lv_textarea_create(ui_panelCalculatorMain);
+  lv_obj_set_align(calculationArea, LV_ALIGN_TOP_MID);
+  lv_obj_set_width(calculationArea, 230);
+  lv_obj_set_height(calculationArea, 35);
+  lv_obj_set_y(calculationArea, -10);
+  lv_obj_add_event_cb(calculationArea, evtComputeCalculations, LV_EVENT_ALL,
+                      NULL);
+  lv_textarea_set_one_line(calculationArea, true);
+
+  calculationResults = lv_textarea_create(ui_panelCalculatorMain);
+  lv_obj_set_align(calculationResults, LV_ALIGN_TOP_MID);
+  lv_obj_set_width(calculationResults, 230);
+  lv_obj_set_height(calculationResults, 60);
+  lv_obj_set_y(calculationResults, 30);
+  lv_obj_add_state(calculationResults, LV_STATE_DISABLED);
+  lv_obj_remove_state(calculationResults, LV_STATE_FOCUSED);
+  lv_obj_remove_flag(calculationResults, LV_OBJ_FLAG_CLICKABLE);  /// Flags
+
+  lv_obj_add_state(calculationArea, LV_STATE_FOCUSED);
+
+  lv_keyboard_set_textarea(kb, calculationArea);
+
   ui_add_bottom_bar(ui_panelCalculatorMain, 0xFFFFFF, 10);
+}
+
+static void evtComputeCalculations(lv_event_t *e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t *ta = lv_event_get_target(e);
+  lv_obj_t *kb = lv_event_get_user_data(e);
+
+  if (code == LV_EVENT_FOCUSED) {
+    // gained focus
+  }
+
+  if (code == LV_EVENT_DEFOCUSED) {
+    // lost focus
+  }
+
+  if (code == LV_EVENT_VALUE_CHANGED) {
+    const char *text = lv_textarea_get_text(calculationArea);
+
+    if (text[strlen(text) - 1] == '=') {
+      // compute the result
+      char result[100];
+    } else if (text[strlen(text) - 1] == 'C') {
+      // clear the text
+      lv_textarea_set_text(calculationArea, "");
+    }
+  }
 }

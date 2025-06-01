a09b0d906b38edaec10c82f296340e75ecca0426
George Ngigi <ngigegeorge023@gmail.com>
2025-06-02 01:22:05 +0300

    Get operator name

--- include/functionality/communication/GSM.hpp
+++ include/functionality/communication/GSM.hpp
@@ -8,12 +8,12 @@
 #include <Utils.hpp>
 
 class GSM {
- private:
-  EspSoftwareSerial::UART *_gsmSerial;  // TODO: Migrate to HardwareSerial
+private:
+  EspSoftwareSerial::UART *_gsmSerial; // TODO: Migrate to HardwareSerial
   bool _responseReady = false, _isRinging = false, _isCallActive = false,
        _isCallAnswered = false, _isCallHungUp = true, _smsSendPending = false;
 
- public:
+public:
   GSM() = delete;
   /**
    * @brief Construct a new GSM object
@@ -21,80 +21,95 @@ class GSM {
    * @param tx TX pin
    * @param baud Baud rate
    */
-  GSM(uint8_t rx, uint8_t tx, uint32_t baud);
+  GSM(int8_t rx, int8_t tx, uint32_t baud);
 
   ~GSM();
 
   /**
    * @brief Initialize the GSM module
    */
-  void init();
+  void init() const;
+
+  /**
+   * @brief Get the name of the network you are connected to
+   * @return Name of the network operator
+   */
+  String getNetworkOperator() const;
+
+  /**
+   * @brief Check the strength of the GSM signal
+   * @return Signal strength, a number between `0` and `31`, where 0 means no
+   * signal and 31 means the best signal.
+   */
+  short getSignalStrength() const;
 
   /**
    * @brief Send an SMS
    * @param number Phone number of the recipient
    * @param message Message
    */
-  void sendSMS(const char *number, const char *message);
+  void sendSMS(const char *number, const char *message) const;
 
   /**
    * @brief Read SMS message
    * @param index Index of the message
    * @param message Buffer to store the message
    */
-  void readSMS(uint8_t index, char *message);
+  void readSMS(uint8_t index, char *message) const;
 
   /**
    * @brief Call a number
-   * @param number Phone number to call
+   * @param number Phone number to call starting with "+"
    */
-  void call(const char *number);
+  void call(const char *number) const;
 
   /**
    * @brief Answer an incoming call
    */
-  void answer();
+  void answer() const;
 
   /**
    * @brief Hang up a call
    */
-  void hangup();
+  void hangup() const;
 
- private:
+private:
   /**
    * @brief Process the response from the GSM module
+   * @param res Buffer to store the response
+   * @param resSize Size of the response buffer
    */
-  void _processResponse();
+  void _processResponse(char *res, unsigned int resSize) const;
 
   /**
    * @brief Send a command to the GSM module
    * @param command Command to send
    */
-  void _sendCommand(const char *command);
-
-  /**
-   * @brief Send a command to the GSM module
-   * @param command Command to send
-   */
-  void _sendCommandW(int value);
-
-  /**
-   * @brief Send a command to the GSM module
-   * @param command Command to send
-   */
-  void _sendCommandLn(const char *command);
+  void _sendCommand(const char *command) const;
 
   /**
    * @brief Send a command to the GSM module
    * @param value Command to send
    */
-  void _sendCommandLn(int value);
+  void _sendCommandW(int value) const;
+
+  /**
+   * @brief Send a command to the GSM module
+   * @param command Command to send
+   */
+  void _sendCommandLn(const char *command) const;
+
+  /**
+   * @brief Send a command to the GSM module
+   * @param value Command to send
+   */
+  void _sendCommandLn(int value) const;
   /**
    * @brief Send a command to the GSM module
    * @param command1 Command to send
    * @param command2 Command to send
    */
-  void _sendCommandLn(const char *command1, const char *command2);
+  void _sendCommandLn(const char *command1, const char *command2) const;
 
   /**
    * @brief Send a command to the GSM module
@@ -103,7 +118,7 @@ class GSM {
    * @param command3 Command to send
    */
   void _sendCommandLn(const char *command1, const char *command2,
-                      const char *command3);
+                      const char *command3) const;
 };
 
-#endif  // GSM_HPP_
+#endif // GSM_HPP_
--- src/fuctionality/communication/GSM.cpp
+++ src/fuctionality/communication/GSM.cpp
@@ -1,132 +1,179 @@
 #include <functionality/communication/GSM.hpp>
 
-GSM::GSM(const uint8_t rx, const uint8_t tx, const uint32_t baud) {
+GSM::GSM(const int8_t rx, const int8_t tx, const uint32_t baud) {
   _gsmSerial = new EspSoftwareSerial::UART(rx, tx);
   _gsmSerial->begin(baud);
 }
 
 GSM::~GSM() { delete _gsmSerial; }
 
-void GSM::init() {
+void GSM::init() const {
   // Send AT command to check if the GSM module is connected
   _sendCommandLn("AT");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Check network signal strength
   _sendCommandLn("AT+CSQ");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Check if the SIM card is inserted
   _sendCommandLn("AT+CPIN?");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Check if the SIM card is ready
   _sendCommandLn("AT+CREG?");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Read network operator
   _sendCommandLn("AT+COPS?");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Read SIM information
   _sendCommandLn("AT+CCID");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Set SMS mode to text
   _sendCommandLn("AT+CMGF=1");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Set SMS storage to SIM
   _sendCommandLn(R"(AT+CPMS="SM","SM","SM")");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // configure SMS to notify the MCU when a new SMS is received
   _sendCommandLn("AT+CNMI=2,2,0,0,0");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   // Set the GSM module to send the caller ID when ringing
   _sendCommandLn("AT+CLIP=1");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   DEBUG_PRINTLN("GSM module initialized");
 }
 
-void GSM::sendSMS(const char* number, const char* message) {
+String GSM::getNetworkOperator() const {
+  _sendCommandLn("AT+COPS?");
+  delay(100);
+  _processResponse(nullptr, 0);
+
+  // The response will be something like: "+COPS: <mode>,<format>,<oper>,<stat>"
+  // where <oper> is the network operator name
+  String response = "";
+  while (_gsmSerial->available()) {
+    response += static_cast<char>(_gsmSerial->read());
+  }
+
+  const int startIndex = response.indexOf("\"") + 1;
+  const int endIndex = response.indexOf("\"", startIndex);
+  return response.substring(startIndex, endIndex);
+}
+
+short
+
+GSM::getSignalStrength() const {
+  _sendCommandLn("AT+CSQ");
+  delay(100);
+  _processResponse(nullptr, 0);
+
+  // The response will be something like: "+CSQ: <rssi>,<ber>"
+  // where <rssi> is the signal strength in dBm
+  String response = "";
+  while (_gsmSerial->available()) {
+    response += static_cast<char>(_gsmSerial->read());
+  }
+
+  const int startIndex = response.indexOf(":") + 1;
+  const int endIndex = response.indexOf(",");
+  const String rssiStr = response.substring(startIndex, endIndex);
+  return static_cast<short>(rssiStr.toInt());
+}
+
+void GSM::sendSMS(const char *number, const char *message) const {
   _sendCommand("AT+CMGS=\"");
   _sendCommand(number);
   _sendCommandLn("\"");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 
   _sendCommand(message);
   _sendCommandW(0x1A);  // End of message character (Ctrl+Z) / ASCII code 26
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 }
 
-void GSM::call(const char* number) {
+void GSM::call(const char *number) const {
   _sendCommand("ATD");  // ATD+<number>; eg. ATD+1234567890;
   _sendCommand(number);
   _sendCommandLn(";");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 }
 
-void GSM::answer() {
+void GSM::answer() const {
   _sendCommandLn("ATA");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 }
 
-void GSM::hangup() {
+void GSM::hangup() const {
   _sendCommandLn("ATH");
   delay(100);
-  _processResponse();
+  _processResponse(nullptr, 0);
 }
 
 // ============================================================================
-void GSM::_processResponse() {
+void GSM::_processResponse(char *res, const unsigned int resSize) const {
   String response = "";
 
   while (_gsmSerial->available()) {
-    response += (char)_gsmSerial->read();
+    response += static_cast<char>(_gsmSerial->read());
   }
   DEBUG_PRINTF("Response: %s\n", response.c_str());
+
+  // put the response to the provided buffer
+  if (res != nullptr) {
+    response.toCharArray(res, resSize);  // Assuming res has enough space
+  }
 }
 
-void GSM::_sendCommand(const char* command) { _gsmSerial->print(command); }
+void GSM::_sendCommand(const char *command) const {
+  _gsmSerial->print(command);
+}
 
-void GSM::_sendCommandW(const int value) { _gsmSerial->write(value); }
+void GSM::_sendCommandW(const int value) const { _gsmSerial->write(value); }
 
-void GSM::_sendCommandLn(const char* command) { _gsmSerial->println(command); }
+void GSM::_sendCommandLn(const char *command) const {
+  _gsmSerial->println(command);
+}
 
-void GSM::_sendCommandLn(const int value) { _gsmSerial->println(value); }
+void GSM::_sendCommandLn(const int value) const { _gsmSerial->println(value); }
 
-void GSM::_sendCommandLn(const char* command1, const char* command2) {
+void GSM::_sendCommandLn(const char *command1, const char *command2) const {
   _gsmSerial->print(command1);
   _gsmSerial->println(command2);
 }
 
-void GSM::_sendCommandLn(const char* command1, const char* command2,
-                         const char* command3) {
+void GSM::_sendCommandLn(const char *command1, const char *command2,
+                         const char *command3) const {
   _gsmSerial->print(command1);
   _gsmSerial->print(command2);
   _gsmSerial->println(command3);
 }
 
-void GSM::readSMS(uint8_t index, char* message) {
+void GSM::readSMS(uint8_t index, char *message) const {
   _sendCommand("AT+CMGR=");
   _sendCommandLn(index);
   delay(100);
-  _processResponse();
+  // TODO: Assuming message buffer is 256 bytes. Change this
+  _processResponse(message, 256);
 }
